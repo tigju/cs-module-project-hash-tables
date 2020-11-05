30393a41ee2f2169c5cc051a2ee3e7e1b092bb73
tigju <tigju24@gmail.com>
2020-11-04 21:07:59 -0500

    Lookup table and word count with hash tables

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,8 @@
 # Your code here
+import time
+import math
+import random
+
 
 
 def slowfun_too_slow(x, y):
@@ -9,6 +13,9 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+lookup_table = {}
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
@@ -16,11 +23,25 @@ def slowfun(x, y):
     """
     # Your code here
 
+    x_y = f'x: {x}, y: {y}'
+
+    if x_y in lookup_table:
+        return lookup_table[x_y]
+    else:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+
+        lookup_table[x_y] = v
+        return lookup_table[x_y]
 
 
 # Do not modify below this line!
-
+start = time.time()
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+end = time.time()
+print(end-start)
