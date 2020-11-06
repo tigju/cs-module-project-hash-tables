efcd22632e1c7a867348772c806cb8419898fd6a
tigju <tigju24@gmail.com>
2020-11-05 19:51:54 -0500

    Expensive Sequence with hash table and tuples solution

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,17 @@
 # Your code here
-
+my_dict = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
-
+    my_dict.update({(0,0,0) : 0})
+    if x <=0:
+        return y+z
+    if x > 0:
+        if (x,y,z) in my_dict:
+            return my_dict[(x,y,z)]
+        
+        my_dict[(x,y,z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        return my_dict[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
